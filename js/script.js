6c656466f0349dea8ab2d4d14e7000ff9675482b
Komphet <komphet.me@gmail.com>
2020-04-19 07:47:47 +0700

    Update Iteration

--- js/script.js
+++ js/script.js
@@ -3,11 +3,6 @@ var backgroud = document.getElementById("backgroud");
 var foxObject = document.getElementById("foxObject");
 var foxObjectLeft = getComputedStyle(foxObject).left.replace("px", "");
 var barrierStatus = "ok";
-var foxInfo = {
-    name: "Sammy",
-    hp: "",
-    level: ""
-}
 
 document.addEventListener('keydown', (e) => {
     switch (e.keyCode) {
@@ -144,6 +139,13 @@ function startGame() {
  * Facebook Login
  * 
  * */
+
+var foxInfo = {
+    name: "Sammy",
+    hp: "",
+    level: ""
+}
+
 window.fbAsyncInit = function () {
     FB.init({
         appId: '2658551874377386',
@@ -185,3 +187,106 @@ function setFoxName() {
 $(document).ready(function () {
     setFoxName();
 });
+
+/**
+ * Array & Iteration
+ */
+const items = [
+    {
+        url: "/images/objects/object1.png",
+        hp: -1
+    },
+    {
+        url: "/images/objects/object2.png",
+        hp: -2
+    },
+    {
+        url: "/images/objects/object3.png",
+        hp: -3
+    },
+    {
+        url: "/images/objects/object4.png",
+        hp: -4
+    },
+    {
+        url: "/images/objects/object5.png",
+        hp: -5
+    },
+    {
+        url: "/images/objects/object6.png",
+        hp: -6
+    },
+    {
+        url: "/images/objects/object7.png",
+        hp: -7
+    },
+    {
+        url: "/images/objects/object8.png",
+        hp: -8
+    },
+    {
+        url: "/images/objects/object9.png",
+        hp: -9
+    },
+    {
+        url: "/images/objects/object10.png",
+        hp: -10
+    },
+    {
+        url: "/images/objects/object11.png",
+        hp: -11
+    },
+    {
+        url: "/images/objects/object12.png",
+        hp: 1
+    },
+    {
+        url: "/images/objects/object13.png",
+        hp: 2
+    },
+    {
+        url: "/images/objects/object14.png",
+        hp: 3
+    },
+    {
+        url: "/images/objects/object15.png",
+        hp: 4
+    },
+    {
+        url: "/images/objects/object16.png",
+        hp: 5
+    },
+    {
+        url: "/images/objects/object17.png",
+        hp: 6
+    },
+    {
+        url: "/images/objects/object18.png",
+        hp: 7
+    },
+    {
+        url: "/images/objects/object19.png",
+        hp: 8
+    },
+    {
+        url: "/images/objects/object20.png",
+        hp: 9
+    },
+    {
+        url: "/images/objects/object21.png",
+        hp: 10
+    },
+    {
+        url: "/images/objects/object22.png",
+        hp: 11
+    },
+];
+$(document).ready(function(){
+    for(let i = 0 ; i < items.length; i++){
+        $("#items").append(`
+            <div class="col-sm-3"><img width="50" src="${items[i].url}" /> HP: ${items[i].hp}</div>
+        `);
+    }
+});
+
+
